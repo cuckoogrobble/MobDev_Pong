a39678b1b1f671b50fb46185a33b00b3f795d87f
cuckoogrobble <hokuspokus@protonmail.com>
2020-07-09 00:17:21 +0200

    paddles and ball drawn

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.pong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+import com.example.pong.custiomView.Ball;
+import com.example.pong.custiomView.Paddle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+    Context context;
+    int height = 0;
+    int width = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Getting Height ad Width
+        WindowManager windowmanager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        windowmanager.getDefaultDisplay().getMetrics(displayMetrics);
+        height = Math.round(displayMetrics.heightPixels / displayMetrics.density);
+        width = Math.round(displayMetrics.widthPixels / displayMetrics.density);
+        System.out.println("Height: " + height);
+        System.out.println("Width: " + width);
+
+        //doesnt play much of a role yet
+        Ball ball = new Ball(this);
+        Paddle paddle1 = new Paddle(this , 10, 500);
+        Paddle paddle2 = new Paddle(this, 2500, 500);
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/pong/custiomView/Ball.java
@@ -0,0 +1,58 @@
+package com.example.pong.custiomView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Ball extends View {
+
+
+
+    private static final int BALL_SIZE = 50;
+    private static final int X = 900;
+    private static final int Y = 500;
+
+    public Ball(Context context) {
+        super(context);
+
+        init(null);
+    }
+
+    public Ball(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public Ball(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    public Ball(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    public void init(@Nullable AttributeSet set){
+
+    }
+    @Override
+    protected void onDraw (Canvas canvas){
+
+        Rect rect = new Rect();
+        rect.left = X;
+        rect.top = Y;
+        rect.right = rect.left + BALL_SIZE;
+        rect.bottom = rect.top + BALL_SIZE;
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(rect, paint);
+    }
+}
