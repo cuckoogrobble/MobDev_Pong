32bbac66e99fc5e67eafd1d692de82e7a6a9656a
cuckoogrobble <hokuspokus@protonmail.com>
2020-07-11 20:30:10 +0200

    updated score to 5

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -289,7 +289,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                         public void run() {
                             move();
 
-                            if (scoreLeft == 1 || scoreRight == 1){
+                            if (scoreLeft == 5 || scoreRight == 5){
                                 gameStop();
                             }
                         }
